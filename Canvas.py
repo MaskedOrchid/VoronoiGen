9e25b4448f84a7794eb7dce5386d74818655e998
MaskedOrchid <lelandasaur@fastmail.com>
2026-02-13 23:35:24 -0500

    Added A few Core Classes for the Voronoi Generation and rendering

---
+++ Canvas.py
@@ -0,0 +1,9 @@
+#this class will handle the rendering and drawing of the voronoi
+
+#need to update with past work/ research how to do this
+
+#imports
+
+from VoronoiHandler import VoronoiHandler
+from VoronoiHandler import Poly
+
